828f11ee135616e136d3a7c6e0677b686653f765
Russell Snyder <russell@rustybass.comr>
2023-06-30 01:07:09 +0200

    media page

---
+++ src/app/media/data.ts
@@ -0,0 +1,86 @@
+export const instagramPostEmbeds = [
+  {
+    label: "murphys law post",
+    id: "CqFcLnltOJA",
+  },
+  {
+    label: "muggendorf",
+    id: "CrpvwInti85",
+  },
+  { label: "Food!", id: "CqLw9m1tgDr" },
+  { label: "Golden Moon", id: "CqFcLnltOJA" },
+  { label: "Smiley looking at you", id: "Cp-EP2rtg1a" },
+  { label: "Herzo bar", id: "Cp0VMwhNHnC" },
+];
+
+export const instagramReelEmbeds = [
+  {
+    label: "rehearsal",
+    id: "Cl249UrsmbZ",
+  },
+  {
+    label: "rehearsal",
+    id: "CSRB01_lDJg",
+  },
+  {
+    label: "lake",
+    id: "CQS6DxMKv3A",
+  },
+  {
+    label: "violin",
+    id: "CDbrFugKpg8",
+  },
+  {
+    label: "strum",
+    id: "CDMUEm1qNF9",
+  },
+  {
+    label: "Irish Rover Live",
+    id: "B5LmPiyikG2",
+  },
+  {
+    label: "Fire Dude",
+    id: "B1MEzzBC_s7",
+  },
+  {
+    label: "fields of Athentry with fire",
+    id: "B0RemGjCXfS",
+  },
+];
+
+export const bandcampEmbeds = [
+  {
+    id: "3285839865",
+    link: "https://paddyslastorder.bandcamp.com/album/in-the-middle-of-the-tune",
+    label: "In the middle of the tune by Paddy's Last Order",
+  },
+  {
+    id: "2768487509",
+    link: "https://paddyslastorder.bandcamp.com/album/any-which-way-the-winds-may-blow",
+    label: "Any which way the winds may blow",
+  },
+  {
+    id: "3439254929",
+    link: "https://paddyslastorder.bandcamp.com/album/sick-and-tired-of-workin",
+    label: "Sick and tired of workin'",
+  },
+];
+
+export const facebookVideoEmbeds = [
+  {
+    label: "music base - irish dancing",
+    id: "5487289054731615",
+  },
+  {
+    label: "st. patrick 2015",
+    id: "1013173548750054",
+  },
+  {
+    label: "live at herzo base",
+    id: "1028682210532521",
+  },
+  {
+    label: "Smiley playing violin",
+    id: "1608432932557443",
+  },
+];
